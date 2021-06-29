546cf0c8264e0a286245936db7e0334907eaa0c8
Ricardo Soares <r3soares@gmail.com>
2021-06-28 22:25:58 -0300

    Adicionado calendário ao Agendamento

--- src/agendamento_vtr/lib/app/modules/agendamento/pages/agendamento_page.dart
+++ src/agendamento_vtr/lib/app/modules/agendamento/pages/agendamento_page.dart
@@ -1,28 +1,75 @@
+// Copyright 2019 Aleksander Woźniak
+// SPDX-License-Identifier: Apache-2.0
+
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class AgendamentoPage extends StatefulWidget {
-  final String title;
-  const AgendamentoPage({Key? key, this.title = 'Agendamentos'})
-      : super(key: key);
   @override
-  AgendamentoPageState createState() => AgendamentoPageState();
+  _AgendamentoPageState createState() => _AgendamentoPageState();
 }
 
-class AgendamentoPageState extends State<AgendamentoPage> {
+class _AgendamentoPageState extends State<AgendamentoPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  final kToday = DateTime.now();
+  late DateTime kFirstDay;
+  late DateTime kLastDay;
+
+  _AgendamentoPageState() {
+    kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
+    kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Agendamentos'),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(12),
-            alignment: Alignment.topLeft,
-            child: Container(),
-          )
-        ],
+      body: TableCalendar(
+        rangeSelectionMode: RangeSelectionMode.disabled,
+        headerStyle: HeaderStyle(formatButtonVisible: false),
+        availableCalendarFormats: const {
+          CalendarFormat.month: 'Mês',
+          CalendarFormat.twoWeeks: '2 Semanas',
+          CalendarFormat.week: 'Semana'
+        },
+        locale: 'pt_BR',
+        firstDay: kFirstDay,
+        lastDay: kLastDay,
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          // Use `selectedDayPredicate` to determine which day is currently selected.
+          // If this returns true, then `day` will be marked as selected.
+
+          // Using `isSameDay` is recommended to disregard
+          // the time-part of compared DateTime objects.
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            // Call `setState()` when updating the selected day
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          }
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
       ),
     );
   }
