04cdbbeb0184912b19d0384cdbbc63f4b07be840
Ricardo Martins Brasil Soares <darkrick3@msn.com>
2021-07-30 16:02:45 -0300

    Adicionado eventos no calendario de reagendamento

--- src/agendamento_vtr/lib/app/modules/agendamento/widgets/reagenda_widget.dart
+++ src/agendamento_vtr/lib/app/modules/agendamento/widgets/reagenda_widget.dart
@@ -1,6 +1,10 @@
 import 'package:agendamento_vtr/app/modules/tanque/models/tanque.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
+
+import '../agenda_store.dart';
 
 class ReagendaWidget extends StatefulWidget {
   final Tanque tanque;
@@ -11,12 +15,22 @@ class ReagendaWidget extends StatefulWidget {
 }
 
 class _ReagendaWidgetState extends State<ReagendaWidget> {
+  final agendaStore = Modular.get<AgendaStore>();
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   final kToday = DateTime.now();
   late DateTime kFirstDay;
   late DateTime kLastDay;
+  Map agendasOcupadas = {};
+  final bolinhas = Container(
+      margin: EdgeInsets.all(1),
+      width: 5.0,
+      height: 5.0,
+      decoration: new BoxDecoration(
+        color: Colors.orange,
+        shape: BoxShape.circle,
+      ));
 
   _ReagendaWidgetState() {
     kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
@@ -28,6 +42,7 @@ class _ReagendaWidgetState extends State<ReagendaWidget> {
   void initState() {
     super.initState();
     _selectedDay = widget.tanque.agenda;
+    agendasOcupadas = agendaStore.getAgendasOcupadas();
   }
 
   @override
@@ -75,6 +90,82 @@ class _ReagendaWidgetState extends State<ReagendaWidget> {
         // No need to call `setState()` here
         _focusedDay = focusedDay;
       },
+      calendarBuilders: CalendarBuilders(
+        dowBuilder: (context, day) {
+          if (day.weekday == DateTime.sunday ||
+              day.weekday == DateTime.saturday) {
+            final text = DateFormat.E('pt_BR').format(day);
+            return Center(
+              child: Text(
+                text,
+                style: TextStyle(color: Colors.red),
+              ),
+            );
+          }
+        },
+        defaultBuilder: (context, day, focusedDay) => diaWidget(day),
+        selectedBuilder: (context, day, focusedDay) =>
+            diaSelecionadoWidget(day),
+        todayBuilder: (context, day, focusedDay) => hojeWidget(day),
+      ),
+    );
+  }
+
+  Widget diaWidget(DateTime dia) {
+    if (agendasOcupadas.containsKey(dia)) {
+      var agenda = agendasOcupadas[dia];
+      if (agenda != null) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('${dia.day}'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:
+                    List.generate(agenda.tanques.length, (index) => bolinhas),
+              )
+            ],
+          ),
+        );
+      }
+    }
+    return Center(
+      child: Text('${dia.day}'),
+    );
+  }
+
+  Widget hojeWidget(DateTime dia) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: new BoxDecoration(
+        color: Colors.deepPurple,
+        shape: BoxShape.circle,
+      ),
+      child: Text(
+        'Hoje',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  Widget diaSelecionadoWidget(DateTime dia) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: new BoxDecoration(
+        color: Colors.blueAccent,
+        shape: BoxShape.circle,
+      ),
+      child: Text(
+        '${dia.day}',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+      ),
     );
   }
 }
