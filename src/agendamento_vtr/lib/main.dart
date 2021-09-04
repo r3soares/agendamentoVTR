a95b86de1bf8f4135358c5a78db4b5a1d1cb5914
Ricardo Soares <r3soares@gmail.com>
2021-09-04 12:31:02 -0300

    Removido Firebase do projeto

--- src/agendamento_vtr/lib/main.dart
+++ src/agendamento_vtr/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:agendamento_vtr/app/repositories/database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -6,7 +5,5 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => initializeDateFormatting().then((_) async => {
-      await Database.initDatabase(),
-      runApp(ModularApp(module: AppModule(), child: AppWidget()))
-    });
+void main() => initializeDateFormatting().then(
+    (_) async => {runApp(ModularApp(module: AppModule(), child: AppWidget()))});
