07133aa1dff8a2cf78043abc068cca3c781a103e
Ricardo Martins Brasil Soares <darkrick3@msn.com>
2021-09-17 16:47:11 -0300

    Tratando resposta do http

--- src/agendamento_vtr/lib/app/repositories/infra/api.dart
+++ src/agendamento_vtr/lib/app/repositories/infra/api.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
 
 import 'package:agendamento_vtr/app/repositories/IRepository.dart';
+import 'package:http/http.dart';
 
 class Api implements IRepository {
   static const String endereco = 'https://localhost:44337/api/vtr/';
@@ -8,31 +9,37 @@ class Api implements IRepository {
   Api(this.controller);
   @override
   delete(id) async {
-    final uri = Uri.parse(endereco + controller + id);
-    return (await http.delete(uri)).body;
+    final uri = Uri.parse('$endereco$controller/$id');
+    return _resposta(await http.delete(uri));
   }
 
   @override
   getAll() async {
     final uri = Uri.parse(endereco + controller);
-    return (await http.get(uri)).body;
+    return _resposta(await http.get(uri));
   }
 
   @override
   getById(id) async {
-    final uri = Uri.parse(endereco + controller + id);
-    return (await http.get(uri)).body;
+    final uri = Uri.parse('$endereco$controller/$id');
+    return _resposta(await http.get(uri));
   }
 
   @override
   save(data) async {
     final uri = Uri.parse(endereco + controller);
-    return (await http.post(uri, body: data, headers: {'Content-Type': 'application/json'})).body;
+    return _resposta(await http.post(uri, body: data, headers: {'Content-Type': 'application/json'}));
   }
 
   @override
   update(data) async {
     final uri = Uri.parse(endereco + controller);
-    return (await http.put(uri, body: data, headers: {'Content-Type': 'application/json'})).body;
+    return _resposta(await http.put(uri, body: data, headers: {'Content-Type': 'application/json'}));
+  }
+
+  _resposta(Response req) {
+    if (req.statusCode != 200) return '';
+    if (req.headers['Content-Type'] != 'application/json') return '';
+    return req.body;
   }
 }
