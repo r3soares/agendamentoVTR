bf924d35f780c55e7b5ae6ae42c939e7283994f7
Ricardo Soares <r3soares@gmail.com>
2021-10-03 18:40:46 -0300

    ..

--- src/agendamento_vtr/lib/app/modules/agendamento/agendamento_module.dart
+++ src/agendamento_vtr/lib/app/modules/agendamento/agendamento_module.dart
@@ -1,5 +1,5 @@
 import 'package:agendamento_vtr/app/modules/agendamento/agenda_store.dart';
-import 'package:agendamento_vtr/app/modules/agendamento/pages/agendamento_page.dart';
+import 'package:agendamento_vtr/app/modules/agendamento/pages/main_page.dart';
 import 'package:agendamento_vtr/app/pesquisa_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -12,6 +12,6 @@ class AgendamentoModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(Modular.initialRoute, child: (_, args) => AgendamentoPage()),
+    ChildRoute(Modular.initialRoute, child: (_, args) => MainPage()),
   ];
 }
