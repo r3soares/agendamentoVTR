b01d225b1b38d66fc609d4fc9b0532a4689460cb
Ricardo Soares <r3soares@gmail.com>
2021-11-01 23:21:53 -0300

    Implementado bitrem

---
+++ src/agendamento_vtr/lib/app/modules/agendamento/dialogs/set_bitrem_dialog.dart
@@ -0,0 +1,69 @@
+import 'package:agendamento_vtr/app/modules/agendamento/models/tanque_agendado.dart';
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+
+class SetBitremDialog extends StatelessWidget {
+  final TanqueAgendado agendado;
+  final List<TanqueAgendado> agendados;
+  const SetBitremDialog(this.agendado, this.agendados);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('${agendado.tanque.placaFormatada}'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: const <Widget>[
+            Text('Informe o segundo tanque'),
+          ],
+        ),
+      ),
+      actions: _geraOpcoes(context),
+    );
+  }
+
+  List<Widget> _geraOpcoes(BuildContext context) {
+    List<Widget> opcoes = List.empty(growable: true);
+
+    for (var i in agendados) {
+      if (i == agendado) continue;
+      var widget = TextButton(
+        child: Text(
+          '${i.tanque.placa}',
+          style: TextStyle(color: Colors.green),
+        ),
+        onPressed: () {
+          agendado.bitremAgenda = i.id;
+          i.bitremAgenda = agendado.id;
+          Navigator.of(context).pop();
+          //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Veículo confirmado')));
+        },
+      );
+      opcoes.add(widget);
+    }
+
+    if (agendado.bitremAgenda != null) {
+      var removerBitrem = TextButton(
+          child: const Text(
+            'Remover Bitrem',
+            style: TextStyle(color: Colors.red),
+          ),
+          onPressed: () {
+            _removeBitrem();
+            Navigator.of(context).pop();
+            //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Veículo confirmado')));
+          });
+      opcoes.add(removerBitrem);
+    }
+
+    return opcoes;
+  }
+
+  _removeBitrem() {
+    var ta2 = agendados.firstWhereOrNull((e) => e.id == agendado.bitremAgenda);
+    if (ta2 != null) {
+      ta2.bitremAgenda = null;
+    }
+    agendado.bitremAgenda = null;
+  }
+}
--- src/agendamento_vtr/lib/app/modules/agendamento/widgets/agenda_do_dia_widget.dart
+++ src/agendamento_vtr/lib/app/modules/agendamento/widgets/agenda_do_dia_widget.dart
@@ -4,6 +4,7 @@ import 'package:agendamento_vtr/app/models/tanque.dart';
 import 'package:agendamento_vtr/app/modules/agendamento/controllers/agendaController.dart';
 import 'package:agendamento_vtr/app/modules/agendamento/dialogs/altera_status_agenda_dialog.dart';
 import 'package:agendamento_vtr/app/modules/agendamento/dialogs/altera_status_dialog.dart';
+import 'package:agendamento_vtr/app/modules/agendamento/dialogs/set_bitrem_dialog.dart';
 import 'package:agendamento_vtr/app/modules/agendamento/dialogs/visualiza_tanque_dialog.dart';
 import 'package:agendamento_vtr/app/modules/agendamento/models/agenda.dart';
 import 'package:agendamento_vtr/app/modules/agendamento/models/tanque_agendado.dart';
@@ -66,81 +67,117 @@ class AgendaDoDiaWidget extends StatelessWidget {
                 flex: 6,
                 child: ScrollConfiguration(
                   behavior: CustomScrollBehavior(),
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      dragStartBehavior: DragStartBehavior.start,
-                      scrollDirection: Axis.vertical,
-                      itemCount: agenda.tanquesAgendados.length,
-                      itemBuilder: (BuildContext ctx, int index) {
-                        TanqueAgendado tAgendado = agenda.tanquesAgendados.elementAt(index);
-                        final status = _getCorConfirmacao(tAgendado.statusConfirmacao);
-                        final isNovo = _isNovo(tAgendado.isNovo);
-                        Tanque tanque = tAgendado.tanque;
-                        return Card(
-                          elevation: 12,
-                          child: ListTile(
-                              leading: TextButton(
-                                  onPressed: () => {
-                                        showDialog(
-                                            barrierDismissible: true,
-                                            barrierColor: Color.fromRGBO(0, 0, 0, .5),
-                                            useSafeArea: true,
-                                            context: context,
-                                            builder: (_) => VisualizaTanqueDialog(tanque)),
-                                      },
-                                  child: Icon(Icons.remove_red_eye)),
-                              title: Row(children: [
-                                Text(
-                                  '${tanque.placaFormatada}',
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                                  child: Text(
-                                    '${tanque.capacidadeTotal} L',
-                                    style: TextStyle(fontSize: 12),
-                                  ),
-                                ),
-                              ]),
-                              subtitle: Text('Registrado em ${Constants.formatoData.format(tanque.dataRegistro)}'),
-                              trailing: ConstrainedBox(
-                                constraints: BoxConstraints.loose(Size(200, double.infinity)),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    TextButton(
-                                        onPressed: () => showDialog(
-                                            context: context,
-                                            builder: (BuildContext context) {
-                                              return AlteraStatusDialog(tAgendado);
-                                            }).then((_) async => await _salvaAlteracoes(context, tAgendado)),
-                                        child: Icon(
-                                          status.value,
-                                          color: status.key,
-                                        )),
-                                    TextButton(
-                                        onPressed: () => {
-                                              tAgendado.isNovo = !tAgendado.isNovo,
-                                              _salvaAlteracoes(context, tAgendado),
-                                            },
-                                        child: Text(
-                                          'Zero',
-                                          style: TextStyle(color: isNovo),
-                                        )),
-                                    TextButton(
-                                        onPressed: () => {
-                                              _excluiTanqueAgendado(tAgendado),
-                                              _salvaAlteracoes(context, tAgendado),
-                                              _notificaPendentes(tAgendado),
-                                            },
-                                        child: Icon(Icons.close)),
-                                  ],
-                                ),
-                              )),
-                        );
-                      }),
+                  child: listaAgendados(context),
                 )),
       ],
     );
+  }
+
+  Widget listaAgendados(BuildContext context) {
+    List<int> pular = [];
+    List<Widget> lista = List.empty(growable: true);
+    for (int i = 0; i < agenda.tanquesAgendados.length; i++) {
+      if (pular.contains(i)) continue;
+      var widget = agenda.tanquesAgendados[i].bitremAgenda == null
+          ? Card(elevation: 12, child: cardWidget(context, i))
+          : Card(
+              elevation: 12,
+              child: Column(
+                children: [
+                  cardWidget(context, i),
+                  getBitrem(context, agenda.tanquesAgendados[i].bitremAgenda!, pular),
+                ],
+              ));
+      lista.add(widget);
+    }
+    return ListView(
+      shrinkWrap: true,
+      dragStartBehavior: DragStartBehavior.start,
+      scrollDirection: Axis.vertical,
+      children: lista,
+    );
+  }
+
+  Widget getBitrem(BuildContext context, String bitrem, List<int> pular) {
+    int index = agenda.tanquesAgendados.indexWhere((e) => e.id == bitrem);
+    pular.add(index);
+    return index != -1 ? cardWidget(context, index) : SizedBox.shrink();
+  }
+
+  Widget cardWidget(BuildContext context, int index) {
+    TanqueAgendado tAgendado = agenda.tanquesAgendados.elementAt(index);
+    final status = _getCorConfirmacao(tAgendado.statusConfirmacao);
+    final isNovo = _isNovo(tAgendado.isNovo);
+    final isBitrem = _isNovo(tAgendado.bitremAgenda != null);
+    Tanque tanque = tAgendado.tanque;
+    return ListTile(
+        leading: TextButton(
+            onPressed: () => {
+                  showDialog(
+                      barrierDismissible: true,
+                      barrierColor: Color.fromRGBO(0, 0, 0, .5),
+                      useSafeArea: true,
+                      context: context,
+                      builder: (_) => VisualizaTanqueDialog(tanque)),
+                },
+            child: Icon(Icons.remove_red_eye)),
+        title: Row(children: [
+          Text(
+            '${tanque.placaFormatada}',
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              '${tanque.capacidadeTotal} L',
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
+        ]),
+        subtitle: Text('Registrado em ${Constants.formatoData.format(tanque.dataRegistro)}'),
+        trailing: ConstrainedBox(
+          constraints: BoxConstraints.loose(Size(300, double.infinity)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                  onPressed: () => showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlteraStatusDialog(tAgendado);
+                      }).then((_) async => await _salvaAlteracoes(context, tAgendado)),
+                  child: Icon(
+                    status.value,
+                    color: status.key,
+                  )),
+              TextButton(
+                  onPressed: () => {
+                        tAgendado.isNovo = !tAgendado.isNovo,
+                        _salvaAlteracoes(context, tAgendado),
+                      },
+                  child: Text(
+                    'Zero',
+                    style: TextStyle(color: isNovo),
+                  )),
+              TextButton(
+                  onPressed: () => showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return SetBitremDialog(tAgendado, agenda.tanquesAgendados);
+                      }).then((_) async => await _salvaAlteracoes(context, tAgendado)),
+                  child: Text(
+                    'Bitrem',
+                    style: TextStyle(color: isBitrem),
+                  )),
+              TextButton(
+                  onPressed: () => {
+                        _excluiTanqueAgendado(tAgendado),
+                        _salvaAlteracoes(context, tAgendado),
+                        _notificaPendentes(tAgendado),
+                      },
+                  child: Icon(Icons.close)),
+            ],
+          ),
+        ));
   }
 
   MapEntry<String, Color> _getCorStatusAgenda(StatusAgenda status) {
