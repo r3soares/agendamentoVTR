938bb270151c35e12cb723120f197ba5ca0d5945
Ricardo Martins Brasil Soares <darkrick3@msn.com>
2021-12-02 16:11:28 -0300

    Adicionado histórico de agendamentos do veículo

--- src/agendamento_vtr/lib/app/modules/agendamento/dialogs/tanque_agendado/historico_tab.dart
+++ src/agendamento_vtr/lib/app/modules/agendamento/dialogs/tanque_agendado/historico_tab.dart
@@ -1,9 +1,11 @@
+import 'package:agendamento_vtr/app/behaviors/custom_scroll_behavior.dart';
 import 'package:agendamento_vtr/app/domain/erros.dart';
+import 'package:agendamento_vtr/app/models/tanque.dart';
 import 'package:agendamento_vtr/app/modules/agendamento/models/tanque_agendado.dart';
 import 'package:agendamento_vtr/app/repositories/repository_tanque.dart';
-import 'package:agendamento_vtr/app/repositories/repository_tanque_agendado.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:agendamento_vtr/app/domain/extensions.dart';
 
 class HistoricoTab extends StatefulWidget {
   final TanqueAgendado tAgendado;
@@ -14,6 +16,7 @@ class HistoricoTab extends StatefulWidget {
 }
 
 class _HistoricoTabState extends State<HistoricoTab> {
+  final ScrollController scrollController = ScrollController();
   final RepositoryTanque repoTanque = Modular.get<RepositoryTanque>();
 
   @override
@@ -41,7 +44,10 @@ class _HistoricoTabState extends State<HistoricoTab> {
         }
         if (historico.hasData) {
           return Center(
-            child: buildHistorico(historico.data!),
+            child: SizedBox(
+              height: 200,
+              child: buildHistorico(historico.data!),
+            ),
           );
         }
         if (historico.hasError) {
@@ -55,5 +61,103 @@ class _HistoricoTabState extends State<HistoricoTab> {
     );
   }
 
-  buildHistorico(List<TanqueAgendado> historico) {}
+  buildCriacaoVeiculo(Tanque t) {
+    return Card(
+      elevation: 10,
+      shadowColor: Colors.black,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              'Veículo criado:\n${t.dataRegistro.diaMesAnoToString()}',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              'Última alteração:\n${t.dataUltimaAlteracao.diaMesAnoToString()}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              '${t.obs ?? ''}',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildHistorico(List<TanqueAgendado> historico) {
+    historico.sort((a, b) => a.dataRegistro.compareTo(b.dataRegistro));
+    return ScrollConfiguration(
+      behavior: CustomScrollBehavior(),
+      child: ListView.builder(
+          itemExtent: 200,
+          physics: const ClampingScrollPhysics(),
+          controller: scrollController,
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: historico.length,
+          itemBuilder: (BuildContext context, int i) {
+            TanqueAgendado ta = historico.elementAt(i);
+            var status = getStatusConfirmacao(ta);
+            return Card(
+              elevation: 10,
+              shadowColor: Colors.black,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      '${ta.agenda == null ? 'Não agendado' : ta.agenda!.replaceAll('-', '/')}',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('${status.key}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: status.value,
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      '${ta.obs ?? ''}',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
+    );
+  }
+
+  MapEntry<String, Color> getStatusConfirmacao(TanqueAgendado ta) {
+    var status = ta.statusConfirmacao;
+    switch (status) {
+      case StatusConfirmacao.PreAgendado:
+        return MapEntry('Fila de Espera', Colors.deepPurple);
+      case StatusConfirmacao.NaoConfirmado:
+        return MapEntry('Não confirmou', Colors.orange);
+      case StatusConfirmacao.Confirmado:
+        return MapEntry('Verificado', Colors.green);
+      case StatusConfirmacao.Reagendado:
+        return MapEntry('Reagendado', Colors.blue.shade900);
+      case StatusConfirmacao.Cancelado:
+        return MapEntry('Cancelado', Colors.red);
+    }
+  }
 }
