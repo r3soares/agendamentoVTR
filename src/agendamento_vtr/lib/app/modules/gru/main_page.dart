c9d844bb28f91612c1738c6c356af235489c8d12
Ricardo Martins Brasil Soares <darkrick3@msn.com>
2021-12-10 16:28:26 -0300

    Agora mapa de municipios é gerada no main page

--- src/agendamento_vtr/lib/app/modules/gru/main_page.dart
+++ src/agendamento_vtr/lib/app/modules/gru/main_page.dart
@@ -1,9 +1,16 @@
+import 'package:agendamento_vtr/app/domain/custo_compartimento.dart';
+import 'package:agendamento_vtr/app/domain/erros.dart';
+import 'package:agendamento_vtr/app/domain/log.dart';
 import 'package:agendamento_vtr/app/models/tanque.dart';
+import 'package:agendamento_vtr/app/modules/agendamento/models/municipio.dart';
 import 'package:agendamento_vtr/app/modules/agendamento/stores/store_data.dart';
+import 'package:agendamento_vtr/app/modules/gru/models/tanque_servico.dart';
 import 'package:agendamento_vtr/app/modules/gru/widgets/gera_pdf_widget.dart';
 import 'package:agendamento_vtr/app/modules/gru/widgets/lista_veiculos_widget.dart';
 import 'package:agendamento_vtr/app/modules/gru/widgets/pesquisa_tanque.dart';
+import 'package:agendamento_vtr/app/repositories/repository_municipio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class MainPage extends StatelessWidget {
   final StoreData<Tanque> resultadoPesquisa = StoreData(Tanque());
@@ -32,7 +39,7 @@ class MainPage extends StatelessWidget {
                     padding: const EdgeInsets.all(8),
                     child: ElevatedButton(
                       child: Text('Gerar Relatório'),
-                      onPressed: () => geraPdf(),
+                      onPressed: () async => await geraPdf(context),
                     ),
                   )
                 ],
@@ -54,8 +61,42 @@ class MainPage extends StatelessWidget {
         )));
   }
 
-  geraPdf() {
-    GeraPdfWidget pdf = GeraPdfWidget(lista);
+  geraPdf(BuildContext context) async {
+    var valida = validaLista();
+    if (valida != null) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(valida)));
+      return;
+    }
+    var custo = CustoCompartimento();
+    Map<int, Municipio> mapa = await getMapMunicipios(context);
+    var ts = TanqueServico(lista, custo, mapa);
+    GeraPdfWidget pdf = GeraPdfWidget(ts);
     pdf.geraPdf();
+  }
+
+  getMapMunicipios(BuildContext context) async {
+    try {
+      final RepositoryMunicipio repoMunicipio = Modular.get<RepositoryMunicipio>();
+      Map<int, Municipio> mapa = Map();
+      for (var tanque in lista) {
+        var codMun = tanque.proprietario!.proprietario!.codMun;
+        mapa[codMun] = await repoMunicipio.getMunicipio(codMun);
+      }
+      return mapa;
+    } on Falha catch (e) {
+      Log.message(this, 'Erro ao buscar municipios para a lista de tanques: ${e.msg}');
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Erro ao buscar municipios para a lista de tanques')));
+    }
+  }
+
+  validaLista() {
+    for (var t in lista) {
+      if (t.proprietario == null) return '${t.placa}: Sem empresa associada';
+      if (t.proprietario!.proprietario == null) return '${t.placa}: Sem proprietário associado';
+      if (t.proprietario!.proprietario!.cod == 0) return '${t.placa}: Sem código de proprietário associado';
+      if (t.proprietario!.proprietario!.codMun == 0) return '${t.placa}: Sem código de município associado';
+    }
+    return null;
   }
 }
