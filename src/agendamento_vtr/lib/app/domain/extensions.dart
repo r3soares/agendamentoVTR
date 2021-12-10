d71e161655f6e6e960723bc0c7673b5ecc4d7770
Ricardo Martins Brasil Soares <darkrick3@msn.com>
2021-12-10 16:26:14 -0300

    Adicionado extensao para string

--- src/agendamento_vtr/lib/app/domain/extensions.dart
+++ src/agendamento_vtr/lib/app/domain/extensions.dart
@@ -21,3 +21,8 @@ extension DatetimeEx on DateTime {
     return '${this.year}-${this.month}-${this.day}';
   }
 }
+
+extension StringCasingExtension on String {
+  String toCapitalized() => length > 0 ? '${this[0].toUpperCase()}${substring(1)}' : '';
+  String toTitleCase() => replaceAll(RegExp(' +'), ' ').split(' ').map((str) => str.toCapitalized()).join(' ');
+}
