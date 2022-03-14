86005752283ba06a1d925eb10460b4a307425b41
Ricardo M B Soares <r3soares@gmail.com>
2022-03-14 10:26:05 -0300

    Adicionado biblioteca HTTP Override para fazer uso de certificados

--- src/agendamento_vtr/lib/main.dart
+++ src/agendamento_vtr/lib/main.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
+import 'app/behaviors/override_http_protocol.dart';
 
-void main() => initializeDateFormatting().then(
-    (_) async => {runApp(ModularApp(module: AppModule(), child: AppWidget()))});
+void main() => initializeDateFormatting().then((_) async => {
+      HttpOverrides.global = new MyHttpOverrides(),
+      runApp(ModularApp(module: AppModule(), child: AppWidget())),
+    });
