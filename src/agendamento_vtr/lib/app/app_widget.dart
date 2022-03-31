a3b9310964dcaa1710d24a6f2191b6b1ae2a6f73
Ricardo M B Soares <r3soares@gmail.com>
2022-03-31 16:40:09 -0300

    Adicionado localização Não funcional

--- src/agendamento_vtr/lib/app/app_widget.dart
+++ src/agendamento_vtr/lib/app/app_widget.dart
@@ -1,15 +1,26 @@
 import 'package:agendamento_vtr/app/behaviors/custom_scroll_behavior.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // localizationsDelegates: [
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   GlobalCupertinoLocalizations.delegate,
+      // ],
+      // supportedLocales: [
+      //   Locale('pt', 'BR'),
+      //   Locale('en', ''),
+      // ],
       scrollBehavior: CustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
       title: 'Agendamento VTR',
-      theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.light),
+      theme:
+          ThemeData(primarySwatch: Colors.blue, brightness: Brightness.light),
     ).modular();
   }
 }
